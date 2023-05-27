24077c8293310559b71f0d0f8f9f0b3e1042d813
vlobolochkov <vlobolochkov@gmail.com>
2023-05-27 22:43:38 +0300

    final tablet layout

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -11,3 +11,18 @@ var swiper = new Swiper('.mySwiper', {
   mousewheel: true,
   keyboard: true
 })
+
+var swiper = new Swiper('.main-price__swiper', {
+  spaceBetween: 0,
+  slidesPerView: 1,
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: false,
+  keyboard: false
+})
